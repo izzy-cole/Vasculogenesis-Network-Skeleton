4bdeb041bf2d1a86333a379a2173d778b3b2d7bb
IzzyCole <ic419@cam.ac.uk>
2026-07-02 16:06:13 +0100

    Standardise network information datastructure

--- analysis.py
+++ analysis.py
@@ -2,6 +2,113 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import networkx as nx
 import pandas as pd
+import numpy as np
+
+
+#Change "4" to be the pixel_micron ratio
+def init_network_info_2d(nodes_list,adj_list,conditions):
+
+    properties = ["Number of Nodes","Mean Edge Length","Mean Node Weight","Average Degree of Non-Isolated Nodes","Number of Basis Cycles","Number of Components","Average Clustering","Average Shortest Path","Number of Isolated Nodes","Number of Components, Excluding Isolated Nodes","Proportion of Isolated Nodes"]
+
+    #2d datastructure
+    df = pd.DataFrame(index=properties, columns=conditions)
+
+    for i in range(len(conditions)):
+        cond=conditions[i]
+        print(f"Condition is: {cond}")
+
+        nodes = nodes_list[i]
+        adj = adj_list[i]
+
+        adj = pd.DataFrame(adj)
+        nodes = pd.DataFrame(nodes)
+
+        df.loc["Number of Nodes",cond] = len(nodes)
+        df.loc["Mean Edge Length",cond] = adj.values[adj.values>0].mean()
+        df.loc["Mean Node Weight",cond] = np.mean(nodes["weight"])
+        df.loc["Average Degree of Non-Isolated Nodes",cond] = count_neighbours(adj,min=1).mean()
+        df.loc["Number of Isolated Nodes",cond] = np.array([count_neighbours(adj,min=0,max=100)==0]).sum()
+
+        G=gen_networkx_graph(nodes,adj)
+        print(f"Network generation complete\n")
+
+
+        largest_cc = max(nx.connected_components(G), key=len)
+        G_comp = G.subgraph(largest_cc).copy()
+
+
+        df.loc["Number of Basis Cycles",cond] = len(sorted(nx.cycle_basis(G)))
+        df.loc["Number of Components",cond] = len(sorted(nx.connected_components(G)))
+        df.loc["Average Clustering",cond] = nx.average_clustering(G)
+        df.loc["Average Shortest Path",cond] = nx.average_shortest_path_length(G_comp, weight='weight') #average shortest path of the largest component
+
+
+        df.loc["Number of Components, Excluding Isolated Nodes",cond] = df.loc["Number of Components",cond] - df.loc["Number of Isolated Nodes",cond]
+        df.loc["Proportion of Isolated Nodes", cond] = df.loc["Number of Isolated Nodes",cond] / df.loc["Number of Nodes",cond]
+
+        xmin = nodes["x"].quantile(0.01)
+        xmax = nodes["x"].quantile(0.99)
+        df.loc["Mean Edge Length, Standardised",(stage,n)] = df.loc["Mean Edge Length",(stage,n)]/ (4*(xmax-xmin))
+
+    return df
+
+#Change "4" to be the pixel_micron ratio
+def init_network_info_3d(nodes_all_stages,adj_all_stages,stages,conditions):
+    properties = ["Number of Nodes","Mean Edge Length","Mean Node Weight","Average Degree of Non-Isolated Nodes","Number of Basis Cycles","Number of Components","Average Clustering","Average Shortest Path","Number of Isolated Nodes","Number of Components, Excluding Isolated Nodes","Proportion of Isolated Nodes"]
+    max_n = 5
+    n_list = range(max_n)
+
+    #3d datastructure
+    multi_cols = pd.MultiIndex.from_product([stages, n_list], names=['Stage', 'n'])
+    df = pd.DataFrame(index=properties, columns=multi_cols)
+
+
+    #set up properties that can be calculated directly
+
+    n_node_weights = []
+    n_neighbours_dists = []
+    stage_index=0
+    for i in range(len(stages)):
+        stage=stages[i]
+
+        for n in range(len(nodes_all_stages[i])):
+            print(f"Stage: {stage}, n: {n}")
+            nodes = nodes_all_stages[i][n]
+            adj = adj_all_stages[i][n]
+
+
+            adj = pd.DataFrame(adj)
+            nodes = pd.DataFrame(nodes)
+
+            df.loc["Number of Nodes",(stage,n)] = len(nodes)
+            df.loc["Mean Edge Length",(stage,n)] = adj.values[adj.values>0].mean()
+            df.loc["Mean Node Weight",(stage,n)] = np.mean(nodes["weight"])
+            df.loc["Average Degree of Non-Isolated Nodes",(stage,n)] = count_neighbours(adj,min=1).mean()
+            df.loc["Number of Isolated Nodes",(stage,n)] = np.array([count_neighbours(adj,min=0,max=100)==0]).sum()
+
+
+            G=gen_networkx_graph(nodes,adj)
+            print(f"Network generation complete\n")
+
+
+            largest_cc = max(nx.connected_components(G), key=len)
+            G_comp = G.subgraph(largest_cc).copy()
+
+
+            df.loc["Number of Basis Cycles",(stage,n)] = len(sorted(nx.cycle_basis(G)))
+            df.loc["Number of Components",(stage,n)] = len(sorted(nx.connected_components(G)))
+            df.loc["Average Clustering",(stage,n)] = nx.average_clustering(G)
+            df.loc["Average Shortest Path",(stage,n)] = nx.average_shortest_path_length(G_comp, weight='weight') #average shortest path of the largest component
+
+
+            df.loc["Number of Components, Excluding Isolated Nodes",(stage,n)] = df.loc["Number of Components",(stage,n)] - df.loc["Number of Isolated Nodes",(stage,n)]
+            df.loc["Proportion of Isolated Nodes", (stage,n)] = df.loc["Number of Isolated Nodes",(stage,n)] / df.loc["Number of Nodes",(stage,n)]
+
+            xmin = nodes["x"].quantile(0.01)
+            xmax = nodes["x"].quantile(0.99)
+            df.loc["Mean Edge Length, Standardised",(stage,n)] = df.loc["Mean Edge Length",(stage,n)]/ (4*(xmax-xmin))
+
+
 
 def mean_line(feature,df,stages):
 
