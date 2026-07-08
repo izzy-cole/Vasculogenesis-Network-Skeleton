dba9cf7dbddbeed6fa32ca5f702bd24d91328764
IzzyCole <ic419@cam.ac.uk>
2026-07-08 13:53:09 +0100

    Add comments

--- database.py
+++ database.py
@@ -14,11 +14,13 @@ def initialise_metadata():
     return metadata_df
 
 def get_embryo_ID(metadata_df,stage,n,condition=None):
+    #Find a match for the embryo ID from the (stage,n,condition) data
     if condition == None or pd.isna(condition):
         index = metadata_df[(metadata_df["Stage"]==stage) & (metadata_df["n"]==n) & (pd.isna(metadata_df["Condition"]))].index
     else:
         index = metadata_df[(metadata_df["Stage"]==stage) & (metadata_df["n"]==n) & (metadata_df["Condition"]==condition)].index
 
+    #return -1 if not found
     if len(index)==0:
         return -1
     if len(index)==1:
@@ -28,8 +30,11 @@ def get_embryo_ID(metadata_df,stage,n,condition=None):
         return -1 
 
 def register_embryos_from_imageJ(metadata_df,drug=np.nan,exp_date=np.nan):
+    #Generates database from .csv files provided by the ImageJ macro skeleton.ijm
     append_count = 0
     skip_count = 0
+
+    #Define the file path
     if pd.isna(drug)  and pd.isna(exp_date):
         file = Path(data_path + "temp/imageJ_metadata.csv")
     else:
@@ -42,6 +47,7 @@ def register_embryos_from_imageJ(metadata_df,drug=np.nan,exp_date=np.nan):
         imageJ_metadata = pd.read_csv(file)
         print(f"Successfully opened metadata file: {file}")
 
+        #Label the drug and experiment date
         imageJ_metadata["Drug"] = drug
         imageJ_metadata["Experiment_Date"] = exp_date
         for i in imageJ_metadata.index:
@@ -52,11 +58,11 @@ def register_embryos_from_imageJ(metadata_df,drug=np.nan,exp_date=np.nan):
             else:
                 id = get_embryo_ID(metadata_df, row["Stage"], row["n"])
 
-            if id == -1:
+            if id == -1: #This embryo is not yet in the database, so add it
                 append_count +=1
                 new_id = int(np.nanmax([0,np.max(metadata_df.index)])+1)
                 metadata_df.loc[new_id] = row
-            else:
+            else: # Embryo already in database
                 skip_count += 1
         print(f"{skip_count} embryos were skipped (already in database), {append_count} new embryos were appended. Total count: {len(metadata_df.index)}")
         return metadata_df
